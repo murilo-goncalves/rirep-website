e358b9702789c92e530d496130a4fc0829270d24
Murilo Gonçalves <murilo@compiuter.local>
2023-06-12 23:49:19 -0300

    finished first website integration

--- src/home.js
+++ src/home.js
@@ -3,75 +3,191 @@ import { rirreperServiceInstance } from './rirreper_service.js'
 class Home {
   constructor() {
     this.rirrepers = [];
-
-    this.modal = document.getElementById("addNoiaModal");
-    this.btn = document.getElementById("myBtn");
-    this.span = document.getElementsByClassName("close")[0];
-    this.span.onclick = () => {
-      this.modal.style.display = "none";
-    }
   }
 
   async init() {
     this.rirrepers = await rirreperServiceInstance.getRirrepers();
 
-    var tableFrag = document.createDocumentFragment();
+    let tableFrag = document.createDocumentFragment();
 
-    for (var i = 0; i < this.rirrepers.length; i += 1) {
-      var column = document.createElement("div");
-      column.className = "column";
-
-      var card = document.createElement("div");
-      card.className = "card";
-
-      var img = document.createElement("img");
-      
-      await rirreperServiceInstance.getRirreperImgPromise(this.rirrepers[i].name)
-        .then((url) => {
-          img.src = url;
-        })
-        .catch((err) => {
-          console.error("Não deu pra pegar essa imagem do banco não, irmão. Deu esse ruim: ", err);
-          alert("deu ruim, olha o log");
-        });
-
-      var container = document.createElement("div");
-      container.className = "cardContainer";
-      tableFrag.appendChild(container);
-
-      var name = document.createElement("h4");
-
-      var bold = document.createElement("b");
-      bold.innerHTML = this.rirrepers[i].name;
-      
-      name.appendChild(bold);
-      container.appendChild(name);
-      // add outras infos
-      card.appendChild(img);
-      card.appendChild(container);
-      column.appendChild(card);
-
+    for (let i = 0; i < this.rirrepers.length; i += 1) {
+      const column = await this.createCard(this.rirrepers[i]);
       tableFrag.appendChild(column);
     }
     
-    let row = document.getElementById("row");
+    const row = document.getElementById("row");
     row.appendChild(tableFrag);
 
-    document.getElementById("btn").onclick = this.clickAddNoia;
+    document.getElementById("new-rirreper-btn").onclick = this.clickNovoRirreper;
+    document.getElementById("vip-selector-btn").onclick = this.changeHtmlToVipSelector;
+    document.getElementById("board-btn").onclick = this.changeHtmlToBoard;
+    document.getElementById("close").onclick = this.closeModal;
+    document.getElementById("submit-btn").onclick = this.clickSalvarRirreper
+    // form.addEventListener("form", this.clickSalvarRirreper);
+    document.addEventListener("click", this.deleteRirreperEventListener);
   }
 
-  clickAddNoia = () => {
-    // if (this.rirrepers.length >= 12) {
-    //   alert("tá maluco de botar mais de 12 noias nessa casa?");
-    // } else {
-      this.modal.style.display = "block";
+  createCard = async (rirreper) => {
+    let column = document.createElement("div");
+    column.className = "column";
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.id = rirreper.name;
+    let img = document.createElement("img");
+    
+    await rirreperServiceInstance.getRirreperImgPromise(rirreper.imgName)
+      .then((url) => {
+        img.src = url;
+      })
+      .catch((err) => {
+        console.error("Não deu pra pegar essa imagem do banco não, irmão. Deu esse ruim: ", err);
+        alert("deu ruim, olha o log");
+      });
+
+    let container = document.createElement("div");
+    container.className = "cardContainer";
+
+    let name = document.createElement("h4");
+
+    let bold = document.createElement("b");
+    bold.innerHTML = rirreper.name;
+    
+    name.appendChild(bold);
+    container.appendChild(name);
+    // add outras infos talvez
+    card.appendChild(img);
+    card.appendChild(container);
+    column.appendChild(card);
+
+    return column;
+  }
+
+  // REMOVER RIRREPER
+
+  deleteRirreperEventListener = async (event) => {
+    if (this.isOnCard(event.composedPath())) {
+      const card = this.getClickedCard(event.composedPath());
+      await this.clickCard(card);
+    }
+  }
+
+  isOnCard = (path) => {
+    return path.some(element => element.className === "card");
+  }
+
+  getClickedCard = (path) => {
+    return path.filter(element => element.className === "card")[0];
+  }
+
+  clickCard = async (card) => {
+    if (confirm(`certeza que quer apagar o ${card.id}?`) == true) {
+      const rirreperName = card.id;
+      const didDelete = await rirreperServiceInstance.deleteRirreper(rirreperName);
+      if (didDelete) {
+        card.parentNode.remove(); // remove column
+        this.rirrepers = await rirreperServiceInstance.getRirrepers();
+      }
+    }
+  }
+
+  // ADD RIRREPER
+
+  clickNovoRirreper = () => {
+    if (this.rirrepers.length >= 12) {
+      alert("tá maluco de botar mais de 12 rirrepers nessa casa?");
+    } else {
+      document.getElementById("addRirreperModal").style.display = "block";
+    }
+  }
+
+  clickSalvarRirreper = async (event) => {
+    const form = document.getElementById("form");
+    const NAME_REQUIRED = "bota o nome do rirreper";
+    const NVIPS_REQUIRED = "bota o número de vips do rirreper";
+    const IMG_REQUIRED = "bota uma imagem do rirreper";
+
+    // stop form submission
+    event.preventDefault();
+    
+    // validate the form
+    const nameValid = this.hasValue(form.elements["name"], NAME_REQUIRED);
+    const nVipsValid = this.hasValue(form.elements["nVips"], NVIPS_REQUIRED);
+    const imgValid = this.hasValue(form.elements["img"], IMG_REQUIRED);
+
+    // if valid, submit the form.
+    if (nameValid && nVipsValid && imgValid) {
+      const zeroFirestoreXArray = { 0: [0, 0, 0],
+                                   1: [0, 0, 0],
+                                   2: [0, 0, 0] };
+      const name = form.elements["name"].value;
+      const nVips = form.elements["nVips"].value;
+      const imgFile = form.elements["img"].files[0];
+
+      console.log(name, nVips, imgFile);
       
-    // }
+      const didSave = await rirreperServiceInstance.saveRirreper(name, zeroFirestoreXArray, nVips, imgFile);  
+
+      if (didSave) {
+        this.closeModal();
+        alert("rirreper add com sucesso!");
+        this.rirrepers = await rirreperServiceInstance.getRirrepers();
+        const rirreper = this.rirrepers.filter(rirreper => rirreper.name === name)[0];
+        const column = await this.createCard(rirreper);
+        const row = document.getElementById("row");
+        row.appendChild(column);
+      }
+
+    }
+  }
+
+  showMessage = (input, message, type) => {
+    // get the small element and set the message
+    const msg = input.parentNode.querySelector("small");
+    msg.innerText = message;
+    // update the class for the input
+    input.className = type ? "success" : "error";
+    return type;
+  }
+
+  showError = (input, message) => {
+    return this.showMessage(input, message, false);
+  }
+  
+  showSuccess = (input) => {
+    return this.showMessage(input, "", true);
+  }
+  
+  hasValue = (input, message) => {
+    if (input.value.trim() === "") {
+      return this.showError(input, message);
+    }
+    return this.showSuccess(input);
+  }
+
+  closeModal = () => {
+    document.getElementById("addRirreperModal").style.display = "none";
+  }
+
+  // ROUTING
+
+  changeHtml = (path) => {
+    window.location.href = `${path}.html`;
+  }
+
+  changeHtmlToVipSelector = () => {
+    this.changeHtml("vip_selector");
+  }
+
+  changeHtmlToBoard = () => {
+    this.changeHtml("board");
   }
 }
 
+
+
 window.onload = () => {
-  let home = new Home();
+  const home = new Home();
     try {
         home.init();
     } catch (err) {
--- src/rirreper_service.js
+++ src/rirreper_service.js
@@ -1,25 +1,21 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { collection, doc, setDoc, getDocs, deleteDoc } from 'firebase/firestore'
+import { getStorage, ref, getDownloadURL, deleteObject, uploadBytes } from "firebase/storage";
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs, deleteDoc } from 'firebase/firestore'
 
 class Rirreper {
-    constructor(name, firestoreArray, nVips) {
+    constructor(name, firestoreXArray, nVips, imgName) {
         this.name = name;
-        this.firestoreArray = firestoreArray;
-        // QUANDO PRECISAR SALVAR NO BANCO NOVAMENTE
-        // this.zeroFirestoreArray = { 0: [0, 0, 0],
-        //                             1: [0, 0, 0],
-        //                             2: [0, 0, 0] };
-        this.xArray = this.getXArrayFromFirestoreArray();
+        this.firestoreXArray = firestoreXArray;
+        this.xArray = this.getXArrayFromFirestoreXArray();
         this.nVips = nVips;
+        this.imgName = imgName;
     }
 
-    getXArrayFromFirestoreArray() {
-        let xArray = [];
-        xArray.push(this.firestoreArray['0']);
-        xArray.push(this.firestoreArray['1']);
-        xArray.push(this.firestoreArray['2']);
+    getXArrayFromFirestoreXArray() {
+        const xArray = [];
+        xArray.push(this.firestoreXArray['0']);
+        xArray.push(this.firestoreXArray['1']);
+        xArray.push(this.firestoreXArray['2']);
         return xArray;
     }
 }
@@ -27,14 +23,12 @@ class Rirreper {
 class RirreperService {
     constructor() {
         const firebaseConfig = {
-
             apiKey: "AIzaSyBdMn31JI0ENjHpdG8ymFFgvNQbYoeZk5g",
             authDomain: "quadro-de-x-ri-rep.firebaseapp.com",
             projectId: "quadro-de-x-ri-rep",
             storageBucket: "quadro-de-x-ri-rep.appspot.com",
             messagingSenderId: "1068317095641",
             appId: "1:1068317095641:web:b463f337238d8c72bfb0c8"
-        
         };
         
         const app = initializeApp(firebaseConfig);
@@ -46,42 +40,35 @@ class RirreperService {
     }
     
     async getRirrepers() {
-        let rirrepers = [];
+        const rirrepers = [];
         const querySnapshot = await getDocs(this.rirrepersRef);
         querySnapshot.forEach((doc) => {
-            let data = doc.data();
-            let rirreper = new Rirreper(data.name, data.xArray, data.nVips)
+            const data = doc.data();
+            const rirreper = new Rirreper(data.name, data.xArray, data.nVips, data.imgName);
             rirrepers.push(rirreper);
         });
         return rirrepers;
     }
 
-    async saveRirreper(rirreper) {
-        try {
-            await setDoc(doc(this.db, "rirrepers", rirreper.name), {
-                name: rirreper.name,
-                nVips: rirreper.nVips,
-                xArray: rirreper.firestoreArray
-            });
-        } catch(err) {
-            console.error("não deu pra salvar não, irmão. Deu esse ruim: ", err);
-            alert("deu ruim, olha o log")
-        }
-    }
-
-    async deleteRirreper(rirreper) {
-        try {
-            await deleteDoc(doc(this.db, "rirrepers", rirreper.name));
-        } catch(err) {
-            console.error("não deu pra deletar não, irmão. Deu esse ruim: ", err);
-            alert("deu ruim, olha o log")
+    async getRirreperByName(name) {
+        const rirreperRef = doc(this.db, "rirrepers", name);
+        const rirreperSnap = await getDoc(rirreperRef);
+        if (rirreperSnap.exists()) {
+            const data = rirreperSnap.data();
+            return new Rirreper(data.name,
+                                data.xArray,
+                                data.nVips,
+                                data.imgName);
+        } else {
+            console.error(`não deu pra achar o rirreper ${name}, irmão. Deu esse ruim: `, err);
+            alert("deu ruim, olha o log");
         }
     }
 
      async updateXArray(rirreper) {
         try {
             await setDoc(doc(this.db, "rirrepers", rirreper.name), {
-                xArray: rirreper.firestoreArray
+                xArray: rirreper.firestoreXArray
             }, { merge: true });
         } catch(err) {
             console.error("não deu pra atualizar o X não, irmão. Deu esse ruim: ", err);
@@ -100,11 +87,62 @@ class RirreperService {
         }
     }
 
-    async getRirreperImgPromise(rirreperName) {
-        let imgRef = ref(this.storage, `${rirreperName}.jpg`);
+    async getRirreperImgPromise(imgName) {
+        const imgRef = ref(this.storage, imgName);
         return getDownloadURL(imgRef);
     }
 
+    async deleteRirreperImg(imgName) {
+        const imgRef = ref(this.storage, imgName);
+        await deleteObject(imgRef).then(() => {
+            console.log(`apagou a imagem ${imgName}}`)
+        }).catch((err) => {
+            console.error("não deu pra apagar a imagem não, irmão. Deu esse ruim: ", err);
+            alert("deu ruim, olha o log");
+        });
+    }
+
+    async saveRirreperImg(imgFile) {
+        const imgRef = ref(this.storage, imgFile.name);
+        await uploadBytes(imgRef, imgFile)
+            .then((snapshot) => {
+                console.log('deu bom salvar a imagem');
+            })
+            .catch(err => {
+                console.error("não deu pra salvar a imagem não, irmão. Deu esse ruim: ", err);
+                alert("deu ruim, olha o log");
+            });    
+    }
+
+    async saveRirreper(name, firestoreXArray, nVips, imgFile) {
+        try {
+            await setDoc(doc(this.db, "rirrepers", name), {
+                name: name,
+                nVips: nVips,
+                xArray: firestoreXArray,
+                imgName: imgFile.name
+            });
+            await this.saveRirreperImg(imgFile);
+            return true;
+        } catch(err) {
+            console.error("não deu pra salvar não, irmão. Deu esse ruim: ", err);
+            alert("deu ruim, olha o log")
+            return false;
+        }
+    }
+
+    async deleteRirreper(rirreperName) {
+        try {
+            const rirreper = await this.getRirreperByName(rirreperName);
+            await deleteDoc(doc(this.db, "rirrepers", rirreperName));
+            await this.deleteRirreperImg(rirreper.imgName);
+            return true;
+        } catch(err) {
+            console.error("não deu pra deletar não, irmão. Deu esse ruim: ", err);
+            alert("deu ruim, olha o log")
+            return false;
+        }
+    }
 }
 
 export const rirreperServiceInstance = new RirreperService();
